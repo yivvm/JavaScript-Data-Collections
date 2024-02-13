92e357680f70556349b55753f1d2db86d4ea5b0c
Yiwen Zhang <yiwenmary@gmail.com>
2024-02-13 18:21:59 -0500

    complete part 3

--- ALAB-308-3-1.js
+++ ALAB-308-3-1.js
@@ -55,9 +55,33 @@ Be careful! If you set n to a number too large, your loop could take a long time
 
 /*
 Part 3: Feeling Loopy ==========================
+Your task is to write a script that accomplishes the following:
+Loop through the characters of a given CSV string.
+Store each “cell” of data in a variable.
+When you encounter a comma, move to the next cell.
+When you encounter the “\r\n” sequence, move to the next “row.”
+Log each row of data.
+You do not need to format the data, the following works well.
+console.log(cell1, cell2, cell3, cell4);
+You can make the following assumptions:
+There will only be 4 cells per row.
+There will be no escaped characters other than “\n”.
 
+Use the example string provided above to test your program. 
+ID,Name,Occupation,Age\n42,Bruce,Knight,41\n57,Bob,Fry Cook,19\n63,Blaine,Quiz Master,58\n98,Bill,Doctor’s Assistant,26
 
-
-
-
+Once you are confident it is working correctly, try the following string to see if your program works properly with other data.
+Index,Mass (kg),Spring 1 (m),Spring 2 (m)\n1,0.00,0.050,0.050\n2,0.49,0.066,0.066\n3,0.98,0.087,0.080\n4,1.47,0.116,0.108\n5,1.96,0.142,0.138\n6,2.45,0.166,0.158\n7,2.94,0.193,0.174\n8,3.43,0.204,0.192\n9,3.92,0.226,0.205\n10,4.41,0.238,0.232
 */
+
+// const str1 = "ID,Name,Occupation,Age\n42,Bruce,Knight,41\n57,Bob,Fry Cook,19\n63,Blaine,Quiz Master,58\n98,Bill,Doctor’s Assistant,26"  // for test
+
+const str1 = "Index,Mass (kg),Spring 1 (m),Spring 2 (m)\n1,0.00,0.050,0.050\n2,0.49,0.066,0.066\n3,0.98,0.087,0.080\n4,1.47,0.116,0.108\n5,1.96,0.142,0.138\n6,2.45,0.166,0.158\n7,2.94,0.193,0.174\n8,3.43,0.204,0.192\n9,3.92,0.226,0.205\n10,4.41,0.238,0.232"
+
+const rows = str1.split('\n')
+console.log(rows)
+
+for (let i = 0; i < rows.length; i++) {
+    const eachRow = rows[i].split(',');
+    console.log(eachRow)
+}
