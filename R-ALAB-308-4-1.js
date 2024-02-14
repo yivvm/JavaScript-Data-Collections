421526e5adf9f877a7ce38f6c5c176ddbab29a65
Yiwen Zhang <yiwenmary@gmail.com>
2024-02-14 08:46:59 -0500

    complete R-ALAB 308.4.1

--- R-ALAB-308-4-1.js
+++ R-ALAB-308-4-1.js
@@ -17,23 +17,23 @@ for (let i = 0; i < rows.length; i++) {
 }
 */
 
-// const csvParsed = (csvString) => {
-//     const rows = csvString.split('\n');
+const csvParse = (csvString) => {
+    const rows = csvString.split('\n');
 
-//     rows.forEach(row => {
-//         const parsed = row.split(',');
-//         // console.log(parsed);
-//         console.log(parsed.join('\t'));
-//     })
-// }
+    rows.forEach(row => {
+        const parsed = row.split(',');
+        // console.log(parsed);
+        console.log(parsed.join('\t'));
+    })
+}
 
-// // const testString = "ID,Name,Occupation,Age\n42,Bruce,Knight,41\n57,Bob,Fry Cook,19\n63,Blaine,Quiz Master,58\n98,Bill,Doctor’s Assistant,26"
+// const testString = "ID,Name,Occupation,Age\n42,Bruce,Knight,41\n57,Bob,Fry Cook,19\n63,Blaine,Quiz Master,58\n98,Bill,Doctor’s Assistant,26"
 
-// // csvParsed(testString);
+// csvParsed(testString);
 
-// massString = 'Index,Mass (kg),Spring 1 (m),Spring 2 (m)\n1,0.00,0.050,0.050\n2,0.49,0.066,0.066\n3,0.98,0.087,0.080\n4,1.47,0.116,0.108\n5,1.96,0.142,0.138\n6,2.45,0.166,0.158\n7,2.94,0.193,0.174\n8,3.43,0.204,0.192\n9,3.92,0.226,0.205\n10,4.41,0.238,0.232';
+massString = 'Index,Mass (kg),Spring 1 (m),Spring 2 (m)\n1,0.00,0.050,0.050\n2,0.49,0.066,0.066\n3,0.98,0.087,0.080\n4,1.47,0.116,0.108\n5,1.96,0.142,0.138\n6,2.45,0.166,0.158\n7,2.94,0.193,0.174\n8,3.43,0.204,0.192\n9,3.92,0.226,0.205\n10,4.41,0.238,0.232';
 
-// csvParsed(massString);
+csvParse(massString);
 
 
 
